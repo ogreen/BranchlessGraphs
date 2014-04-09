cd7ff20e213a44c954cc4b481a52e671c8fe4c48
odedgreen <oded.green@gmail.com>
2014-04-08 23:29:47 -0400

    Improved the script for creating the figures for the paper.

--- allgraphs.R
+++ allgraphs.R
@@ -2,7 +2,7 @@ rm(list = ls())
 library(ggplot2)
 
 for (alg in c("sv")){
-	for (arch in c("arn")) {
+	for (arch in c("arn","hsw")) {
 	  for (graph in c("astro-ph","audikw1","auto","coAuthorsDBLP","coPapersDBLP","cond-mat-2003", 
 	                  "cond-mat-2005","ecology1", "ldoor", "power","preferentialAttachment")){
 		perfdata = read.table(paste(arch, "-",alg,"/", graph, ".log", sep=""), sep="\t")
@@ -10,79 +10,88 @@ for (alg in c("sv")){
 		if(alg=="sv"){
 		  names(perfdata) <- c("dummy","Algorithm", "Iteration", "Time", "Mispredictions", "Branches", "Instructions","Vertices", "Edges")
 			perfdata$Algorithm <- factor(perfdata$Algorithm, levels=unique(perfdata$Algorithm))
-		
+		  phase <-"iteration"
+      Phase <-"Iteration"
 		}
 		else
 		{
   
  			names(perfdata) <- c("dummy","Algorithm", "Iteration", "Time", "Mispredictions", "Branches", "Instructions", "Vertices", "Edges")
  			perfdata$Algorithm <- factor(perfdata$Algorithm, levels=unique(perfdata$Algorithm))
+ 			phase <-"level"    
+      Phase <-"Level"
 		}
     
 		maxIter<-max(perfdata$Iteration)
     
 		timePlot <- 
 		  ggplot(subset(perfdata, Iteration!=0),aes(x=Iteration, y=Time, colour=Algorithm)) +
-		  geom_line() + xlab("Level") + theme(text = element_text(size=20)) +
+		  geom_line() + xlab(Phase) + theme(text = element_text(size=20)) +
 		  ylab("Time (s)") + 
-		  ggtitle("Execution time per level")
+		  ylim(c(0,max(perfdata$Time)))+        
+		  ggtitle(paste("Execution time per ",phase))
 
 #		if(alg=="bfs"){
 		  
 			perfdata$timePerEdge = perfdata$Time / perfdata$Edges
 			timePerEdgePlot <- 
 			  ggplot(subset(perfdata, Iteration!=0),aes(x=Iteration, y=timePerEdge, colour=Algorithm)) +
-			  geom_line() + xlab("Level") + theme(text = element_text(size=20)) +
+			  geom_line() + xlab(Phase) + theme(text = element_text(size=20)) +
 			  ylab("Time (ns)") +
+        ylim(c(0,max(perfdata$timePerEdge)))+    
 			  ggtitle("Averege time per edge travesal")
 #		}		
     
 		branchesPlot <- 
 		  ggplot(subset(perfdata, Iteration!=0),aes(x=Iteration, y=Branches, colour=Algorithm)) +
-		  geom_line() + xlab("Level") + theme(text = element_text(size=20)) +
-		  ylim(c(0,max(perfdata$Branches)))+ 
+		  geom_line() + xlab(Phase) + theme(text = element_text(size=20)) +
       ylab("Branches") +
-		  ggtitle("Branches per level")
+      ylim(c(0,max(perfdata$Branches)))+    
+		  ggtitle(paste("Branches per ", phase))
 
 #		if(alg=="bfs"){
 		  
 			perfdata$BranchesPerEdge = perfdata$Branches / perfdata$Edges
 			branchesPerEdgePlot <- 
 			  ggplot(subset(perfdata, Iteration!=1),aes(x=Iteration, y=BranchesPerEdge, colour=Algorithm)) +
-			  geom_line() + xlab("Level") + theme(text = element_text(size=20)) +
-			  ylim(c(0,1+max(perfdata$BranchesPerEdge)))+ 
+			  geom_line() + xlab(Phase) + theme(text = element_text(size=20)) +
 			  ylab("Branches") +
+        ylim(c(0,max(perfdata$BranchesPerEdge)))+    
 			  ggtitle("Average number of branches \n per edge travesal")
 #		}
 		
 		mispredictionsPlot <- 
 		  ggplot(subset(perfdata, Iteration!=0),aes(x=Iteration, y=Mispredictions, colour=Algorithm)) +
-		  geom_line() + xlab("Level") + theme(text = element_text(size=20)) +
+		  geom_line() + xlab(Phase) + theme(text = element_text(size=20)) +
 		  ylab("Branch Mispredictions") +
-		  ggtitle("Branch mispredictions per level")
+      ylim(c(0,max(perfdata$Mispredictions)))+    
+		  ggtitle(paste("Branch mispredictions per ",phase))
 		
 #		if(alg=="bfs"){
 			perfdata$mispredictionsPerEdge = perfdata$Mispredictions / perfdata$Edges
 			mispredictionsPerEdgePlot <- 
 			  ggplot(subset(perfdata, Iteration!=0),aes(x=Iteration, y=mispredictionsPerEdge, colour=Algorithm)) +
-			  geom_line() + xlab("Level") + theme(text = element_text(size=20)) +
+			  geom_line() + xlab(Phase) + theme(text = element_text(size=20)) +
 			  ylab("Branch Mispredictions") +
+        ylim(c(0,max(perfdata$mispredictionsPerEdge)))+      
 			  ggtitle("Branch mispredictions per \n edge travesal")
 #		}		
     
 		instructionsPlot <- 
 		  ggplot(subset(perfdata, Iteration!=0),aes(x=Iteration, y=Instructions, colour=Algorithm)) +
-		  geom_line() + xlab("Level") + theme(text = element_text(size=20)) +
+		  geom_line() + xlab(Phase) + theme(text = element_text(size=20)) +
 		  ylab("Instructions") +
-		  ggtitle("Instructions per level")
+      ylim(c(0,max(perfdata$Instructions)))+      
+		  ggtitle(paste("Instructions per ",phase))
 
 
 #		if(alg=="bfs"){	
 			perfdata$instructionsPerEdge = perfdata$Instructions / perfdata$Edges
 			instructionsPerEdgePlot <- 
 			  ggplot(subset(perfdata, Iteration!=0),aes(x=Iteration, y=instructionsPerEdge, colour=Algorithm)) +
-			  geom_line() + xlab("Level") + theme(text = element_text(size=20)) +
+			  geom_line() + xlab(Phase) + theme(text = element_text(size=20)) +
 			  ylab("Instructions") +
+        ylim(c(0,max(perfdata$instructionsPerEdge)))+      
 			  ggtitle("Instructions per edge travesal")
 #		}
     
