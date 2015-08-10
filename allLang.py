8cfe4dd274ed45c9c374a531071a748f3f92cb94
Oded Green <ogreen@gatech.edu>
2015-08-10 14:15:47 -0400

    Plotting results is now supported.

--- allLang.py
+++ allLang.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import sys
 import subprocess
-
+from graphNames import graphList
 
 def printGraphList(graphList):
     for g in graphList:
@@ -11,30 +11,6 @@ def main(argv):
     printC=True
     printJava=True
     printPy=True
-  
-    graphList=[]
-    graphList.append(["clustering/","football"])
-    graphList.append(["clustering/","astro-ph"])
-    graphList.append(["clustering/","caidaRouterLevel"])
-    graphList.append(["clustering/","cond-mat-2005"])
-    graphList.append(["clustering/","preferentialAttachment"])
-    graphList.append(["clustering/","smallworld"])
-    graphList.append(["coauthor/","coAuthorsDBLP"])
-#     graphList.append(["coauthor/","coPapersDBLP"])  - Failed python and java
-
-    graphList.append(["delaunay/","delaunay_n20"])
-    graphList.append(["matrix/","audikw1"])
-    graphList.append(["matrix/","cage15"])
-    graphList.append(["matrix/","ldoor"])
-    graphList.append(["walshaw/","auto"])
-    graphList.append(["walshaw/","finan512"])
-    graphList.append(["walshaw/","m14b"])
-    graphList.append(["walshaw/","vibrobox"])
-
-    graphListMin=[]
-    graphListMin.append(["clustering/" ,"football"])
-    graphListMin.append(["clustering/","astro-ph"])
-
 
 #    print graphList
     #graphDir="/home/greenman/data/dimacs/"
@@ -48,11 +24,9 @@ def main(argv):
             if(printJava==True):
                 subprocess.call(["java","cct",graphName,graph[1]],stdout=outfile)
             if(printPy==True):
-				temp1="--ifile="+graphName
-				# print temp1
-				temp2="--gname="+graph[1]
-				# print temp2
-				subprocess.call(["python", "cctmain.py", temp1,temp2 ],stdout=outfile)
+                temp1="--ifile="+graphName
+                temp2="--gname="+graph[1]
+                subprocess.call(["python", "cctmain.py", temp1,temp2 ],stdout=outfile)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
---
+++ plotter.py
@@ -0,0 +1,202 @@
+
+"""
+Bar chart demo with pairs of bars grouped for easy comparison.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+import re 
+from graphNames import graphList
+
+allPlat=["C","Java", "Python"]
+#plotColors=['MediumSpringGreen', 'RebeccaPurple','RoyalBlue','FireBrick','DarkCyan']
+#plotColors=['MediumSpringGreen', 'RebeccaPurple','RoyalBlue','FireBrick','DarkCyan']
+plotColors=['MediumSpringGreen', 'Purple','RoyalBlue','FireBrick','DarkCyan']
+
+syntheticNames=["Increment", "Add Integer", "Add Variable", "Add 1 Condition", "Add 3 Conditions"]
+
+#allPlat=["C"]
+graphLabelRotation = 60
+def parseResultFile(graphResFile):
+    fp = open(graphResFile,"r")
+    allRes=[]
+    for line in fp:
+        line=re.sub('[,]',"", line)
+        res = line.split()
+        newRes=[res[0], res[1]]
+        for r in range (2, len(res)):
+            newRes.append(float(res[r]))
+        
+        allRes.append(newRes)
+    fp.close()
+#    print allRes
+    
+    return allRes;    
+
+def filterLanguages(selLang,allResults):
+    filtered=[]
+    for res in allResults:
+        for plat in res:
+            if (plat[1]==selLang):
+                filtered.append(plat)
+
+    return filtered
+
+def filterColumn(column,allResults):
+    filtered=[]
+    for res in allResults:
+        filtered.append(res[column])
+
+    return filtered
+
+
+def genTimePlots(resDir,results,graphs):
+    for lang in allPlat:
+        filterByLang=filterLanguages(lang, results)
+        timeBB=filterColumn(6, filterByLang)
+        timeBA=filterColumn(7, filterByLang)
+    
+        for t in range (0, len(timeBB)):
+            timeBA[t]=timeBA[t]/timeBB[t]
+            timeBB[t]=1
+    
+        fig, ax = plt.subplots()
+        index = np.arange(len(timeBA))
+        
+        bar_width = 0.35
+    
+        plt.bar(index, timeBB, bar_width,
+                      color='Crimson',
+                      label='Branch-Based')
+    
+        plt.bar(index+bar_width, timeBA, bar_width,
+                      color='DodgerBlue',
+                      label='Branch-Avoiding')
+     
+        plt.ylabel('Time') 
+        plt.title('Normalized time (to Branch-Based)')
+        plt.xticks(index,graphs,rotation=graphLabelRotation)
+        plt.legend( loc=0,    ncol=2, mode="expand", borderaxespad=0.)    
+        plt.ylim(0, 2)
+     
+        plt.tight_layout()
+        
+        figName=resDir+"time"+lang
+        plt.savefig(figName+".pdf", format="pdf");
+        plt.savefig(figName+".png", format="png");
+    return
+
+def genCrossLangCompare(resDir,results,graphs,title,ylabel,column,maxy,fileName, dashedLine):
+    fig, ax = plt.subplots()
+    index = np.arange(len(graphs))
+    bar_width = 0.25
+    langCounter=0
+
+    for lang in allPlat:
+        filterByLang=filterLanguages(lang, results)
+        columnVals=filterColumn(column, filterByLang)
+
+        plt.bar(index+bar_width*langCounter, columnVals, bar_width, 
+                #color="b",
+                      color=plotColors[langCounter],
+                      label=lang)
+        langCounter+=1
+
+    if (dashedLine>0):
+        plt.plot([0,len(index)], [dashedLine,dashedLine],lw=1.2, color='g', linestyle='--')
+
+    plt.ylabel(ylabel) 
+    plt.title(title)
+    plt.xticks(index,graphs,rotation=graphLabelRotation)
+    plt.legend( loc=0,    ncol=3, mode="expand", borderaxespad=0.)    
+    plt.ylim(0, maxy)
+    
+    plt.tight_layout()
+    
+    figName=resDir+fileName
+    plt.savefig(figName+".pdf", format="pdf");
+    plt.savefig(figName+".png", format="png");    
+    return
+
+def genSyntheticCompare(resDir,results,graphs,title,ylabel,columnmin,columnmax,maxy,fileName, dashedLine,lang):
+    fig, ax = plt.subplots()
+    index = np.arange(len(graphs))
+    bar_width = 1.0/(columnmax-columnmin+1)
+    
+    counter=0
+
+    for col in range(columnmin,columnmax):  
+        filterByLang=filterLanguages(lang, results)
+        columnVals=filterColumn(col, filterByLang)
+
+        plt.bar(index+bar_width*counter, columnVals, bar_width, 
+                      color=plotColors[counter], label=syntheticNames[counter])
+        counter+=1
+
+    if (dashedLine>0):
+        plt.plot([0,len(index)], [dashedLine,dashedLine],lw=1.2, color='black', linestyle='--')
+
+
+    plt.ylabel(ylabel) 
+    plt.title(title)
+    plt.xticks(index,graphs,rotation=graphLabelRotation)
+
+    plt.legend( loc=0,ncol=3, mode="expand", borderaxespad=0. )    
+
+    plt.ylim(0, maxy)
+    
+    plt.tight_layout()
+    
+    figName=resDir+fileName
+    plt.savefig(figName+".pdf", format="pdf");
+    plt.savefig(figName+".png", format="png");    
+
+
+    return
+#######
+## MAIN
+#######        
+
+graphNum=len(graphList)
+
+#print graphNum
+
+graphs = []
+results = []
+resDir="res-titan/"
+for graph in graphList:
+    graphs.append(graph[1])
+    graphResFile=resDir+graph[1]+".csv"
+    results.append(parseResultFile(graphResFile))
+#print results
+
+filteredC=filterLanguages("C",results)
+filteredPython=filterLanguages("Python",results)
+filteredJava=filterLanguages("Java",results)
+
+genTimePlots(resDir,results,graphs)
+genCrossLangCompare(resDir,results,graphs,"Percentage of intersection whens Branch-Avoiding is faster","Percentage (%)",8,1.2,"ba-wins",0.5)
+genCrossLangCompare(resDir,results,graphs,"Normalized execution time in comparison with addition/increment","Normalized Execution Time",13,4,"cond3",1.0)
+
+for lang in allPlat:
+    genSyntheticCompare(resDir,results,graphs,"Normalized execution time for synthetic benchmarks","Normalized Execution Time",9,14,4,"synthetic"+lang,1.0,lang)
+#def genSyntheticCompare(resDir,results,graphs,title,ylabel,columnmin,columnmax,maxy,fileName, dashedLine,lang):
+
+
+#    plt.show()
+# opacity = 0.4
+# error_config = {'ecolor': '0.3'}
+# 
+# plt.xlabel('Group')
+# plt.ylabel('Scores')
+# plt.title('Scores by group and gender')
+# plt.xticks(index + bar_width, ('A', 'B', 'C', 'D', 'E'))
+# plt.legend()
+# 
+# plt.tight_layout()
+
+
+#plt.savefig('temp'+".png", format="png")
+#plt.savefig('temp'+".pdf", format="pdf")
+
+
+#plt.show()
